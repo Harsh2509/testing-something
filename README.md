68353a38e4b91432999eab9cdac0008ebdc90056
Harsh <harshchauhan6176@gmail.com>
2023-09-24 16:43:06 +0530

    testing

---
+++ README.md
@@ -0,0 +1,1 @@
+ssh -i ./personal-key-todo.pem ubuntu@ec2-52-90-28-221.compute-1.amazonaws.com
--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,1 @@
-ssh -t -i "cert-file.cer" ubuntu@ec2-url  "sudo bash ~/deploy.sh"
+ssh -t -i "personal-key-todo.pem" ubuntu@ec2-52-90-28-221.compute-1.amazonaws.com "sudo bash ~/deploy.sh"
